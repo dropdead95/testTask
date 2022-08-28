dfe30ccfbd5d945d67f1b4012ec4a26325d17b40
Andrey Lisovsky <mitolcore1234@gmail.com>
2022-08-28 17:44:15 +0300

    resolve 02-collapse

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,30 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".collapsible__button"),
+    show = btn.querySelector(".collapsible__action--hidden"),
+    hide = btn.querySelector(".collapsible__action--visible"),
+    collapsibleContent = document.querySelector(".collapsible__content");
+
+  hide.style.display = "none";
+  collapsibleContent.style.display = "none";
+
+  if (
+    hide.style.display === "none" &&
+    collapsibleContent.style.display === "none"
+  ) {
+    btn.addEventListener("click", (e) => {
+      collapsibleContent.style.display = "block";
+      show.style.display = "none";
+      hide.style.display = "block";
+    });
+  }
+  if (
+    hide.style.display === "block" &&
+    collapsibleContent.style.display === "block"
+  ) {
+    btn.addEventListener("click", (e) => {
+      collapsibleContent.style.display = "none";
+      show.style.display = "block";
+      hide.style.display = "none";
+    });
+  }
+});
