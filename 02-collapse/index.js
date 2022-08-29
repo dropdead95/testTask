411c3e0abb25437ac5dfb70158ea2625eb40f132
Andrey Lisovsky <mitolcore1234@gmail.com>
2022-08-29 17:51:57 +0300

    resolve 02-collapse

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -5,26 +5,30 @@ document.addEventListener("DOMContentLoaded", () => {
     collapsibleContent = document.querySelector(".collapsible__content");
 
   hide.style.display = "none";
-  collapsibleContent.style.display = "none";
-
-  if (
-    hide.style.display === "none" &&
-    collapsibleContent.style.display === "none"
-  ) {
-    btn.addEventListener("click", (e) => {
-      collapsibleContent.style.display = "block";
+  collapsibleContent.style.opacity = 0;
+  btn.addEventListener("click", (e) => {
+    if (e.target === show) {
       show.style.display = "none";
-      hide.style.display = "block";
-    });
-  }
-  if (
-    hide.style.display === "block" &&
-    collapsibleContent.style.display === "block"
-  ) {
-    btn.addEventListener("click", (e) => {
-      collapsibleContent.style.display = "none";
+      collapsibleContent.style.opacity = 1;
+      collapsibleContent.animate(
+        {
+          opacity: [0, 1],
+        },
+        { duration: 1000 }
+      );
+    }
+    hide.style.display = "block";
+
+    if (e.target === hide) {
       show.style.display = "block";
+      collapsibleContent.animate(
+        {
+          opacity: [1, 0],
+        },
+        { duration: 1000 }
+      );
+      collapsibleContent.style.opacity = 0;
       hide.style.display = "none";
-    });
-  }
+    }
+  });
 });
