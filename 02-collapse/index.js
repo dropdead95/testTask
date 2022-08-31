419e676b6de65a8c3983106cbcacde6db1077f84
Andrey Lisovsky <mitolcore1234@gmail.com>
2022-08-31 19:34:20 +0300

    reworked 01-product-card, reworked 02-collapse

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,43 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector(".collapsible__button"),
-    show = btn.querySelector(".collapsible__action--hidden"),
-    hide = btn.querySelector(".collapsible__action--visible"),
-    collapsibleContent = document.querySelector(".collapsible__content");
+const showText = (show, hide, content) => {
+  show.style.display = "none";
+  content.style.opacity = "1";
+  content.animate(
+    {
+      opacity: [0, 1],
+    },
+    { duration: 1000 }
+  );
+  hide.style.display = "block";
+};
 
+const hideText = (show, hide, content) => {
+  show.style.display = "block";
+  content.animate(
+    {
+      opacity: [1, 0],
+    },
+    { duration: 1000 }
+  );
+  content.style.opacity = "0";
   hide.style.display = "none";
-  collapsibleContent.style.opacity = "0";
+};
 
-  function showText() {
-    show.style.display = "none";
-    collapsibleContent.style.opacity = "1";
-    collapsibleContent.animate(
-      {
-        opacity: [0, 1],
-      },
-      { duration: 1000 }
-    );
-    hide.style.display = "block";
-  }
+const collapsibleInit = () => {
+  const collapsible = document.querySelectorAll(".collapsible");
 
-  function hideText() {
-    show.style.display = "block";
-    collapsibleContent.animate(
-      {
-        opacity: [1, 0],
-      },
-      { duration: 1000 }
-    );
-    collapsibleContent.style.opacity = "0";
-    hide.style.display = "none";
-  }
+  collapsible.forEach((elem) => {
+    const button = elem.querySelector(".collapsible__button");
+    const visibleText = button.querySelector(".collapsible__action--hidden");
+    const hiddenText = button.querySelector(".collapsible__action--visible");
+    const content = elem.querySelector(".collapsible__content");
 
-  btn.addEventListener("click", (e) => {
-    if (e.target === show) {
-      showText();
-    }
+    hiddenText.style.display = "none";
+    content.style.opacity = "0";
 
-    if (e.target === hide) {
-      hideText();
-    }
+    button.addEventListener("click", (e) => {
+      if (e.target === visibleText) {
+        showText(visibleText, hiddenText, content);
+      }
+
+      if (e.target === hiddenText) {
+        hideText(visibleText, hiddenText, content);
+      }
+    });
   });
-});
+};
+
+document.addEventListener("DOMContentLoaded", collapsibleInit);
